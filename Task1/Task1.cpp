15f24d8b4a6aa57d39ffd52f83402e0adf631b3b
Stelwart <stelwart.rush@gmail.com>
2022-01-18 05:32:07 +0200

    Добавьте файлы проекта.

---
+++ Task1/Task1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	char c;
+	cout << "Input text" << endl;
+	getline(cin, s);
+
+		vector <int> a;
+		for (char c : s) 
+			a.push_back(c);
+		
+	for (int i = 0; i < s.size(); i++) {
+		int temp = 0;
+		for (int j = 0; j < s.size(); j++) {
+			if (a[j] == a[i] && i != j)
+			temp++;	}
+		if (temp == 0) {
+			cout << '(';}
+		else {
+			cout << ')';}
+	}
+	return 0;
+}
